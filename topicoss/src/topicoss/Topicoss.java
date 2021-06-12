faa8faa979ee5d422a94e8be620c9c26cc135509
josedejesus01 <75240569+josedejesus01@users.noreply.github.com>
2021-06-12 05:06:19 -0500

    Add files via upload

---
+++ topicoss/src/topicoss/Topicoss.java
@@ -0,0 +1,13 @@
+
+package topicoss;
+
+
+public class Topicoss {
+
+   
+    public static void main(String[] args) {
+        formulario1 formulario=new formulario1();//se instancian las clases
+        formulario.setVisible(true);// se manda la funcion para mostra los datos 
+    }
+    
+}
